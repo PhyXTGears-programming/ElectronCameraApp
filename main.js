69a4b23bb3869791d049cf7a1f561ac3ee56d408
PhyXTGears <phyxtgearspro@gmail.com>
2019-04-18 18:46:52 -0400

    Close app when window is closed.
    - Robby S

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const isReachable = require('is-reachable');
 function createWindow () {
     // Create the browser window.
     let win = new BrowserWindow({width: 1200, height: 840, x: 0, y: 0, frame: false})
+    win.on('closed', () => app.quit())
 
     // and load the index.html of the app.
     win.loadFile('index.html')
