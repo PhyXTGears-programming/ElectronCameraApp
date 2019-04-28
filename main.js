4de0cd3804b1d3c3c2deebb87fe7c443d02b0514
PhyXTGears-1720 <phyxtgearspro@gmail.com>
2019-04-27 22:10:16 -0400

    fixme

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+const TEAM_NUMBER = "1720";
+
 const { app, BrowserWindow } = require('electron')
 const isReachable = require('is-reachable');
 
+let win = null;
+
 function createWindow () {
     // Create the browser window.
-    let win = new BrowserWindow({width: 1200, height: 840, x: 0, y: 0, frame: false})
-    win.on('closed', () => app.quit())
+    let win = new BrowserWindow({width: 1200, height: 840, x: 0, y: 0, frame: false, webPreferences: {nodeIntegration: true}})
+    win.on('closed', () => app.quit()) // TODO remove inline function
 
     // and load the index.html of the app.
     win.loadFile('index.html')
@@ -13,6 +17,6 @@ function createWindow () {
     ipc.on("win.close", (e) => {
         win.close();
     });
-}
+};
   
 app.on('ready', createWindow)
