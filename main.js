7518135e04cae1c9ee5df0a8fc864494a046512a
PhyXTGears 1720 <phyxtgearspro@gmail.com>
2020-03-06 20:38:44 -0500

    Set to use an alternate display
    - Logan H-D

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-const { app, BrowserWindow } = require('electron')
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
 const isReachable = require('is-reachable');
 
 function createWindow () {
     // Create the browser window.
-    let win = new BrowserWindow({width: 1200, height: 840, x: 0, y: 0, frame: false})
+	var electronScreen = electron.screen;
+	var displays = electronScreen.getAllDisplays();
+	var externalDisplay = null;
+	for (var i in displays) {
+		if (displays[i].bounds.x != 0 || displays[i].bounds.y != 0) {
+			externalDisplay = displays[i];
+			break;
+		}
+	}
+	
+    let win = new BrowserWindow({
+		width: 1920,
+		height: 800,
+		x:  externalDisplay.bounds.x,
+		y:  externalDisplay.bounds.y,
+		frame: false
+	})
     win.on('closed', () => app.quit())
 
     // and load the index.html of the app.
